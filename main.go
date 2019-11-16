00ffbc3b6c6590d8898d3da1c9f244a256cab396
stevenstr <stephenzhavrid@gmail.com>
2019-11-16 18:24:22 +0300

    Update main.go
    Home work 2.2 Implement the following methods: End - defines the end of the shape, Perimiter, Area

--- main.go
+++ main.go
@@ -5,7 +5,40 @@
 **/
 
 package main
+
 import "fmt"
-func main(){
-  fmt.Println("Hello Golang World!")
+
+//Point structure
+type Point struct {
+	x, y int
+}
+
+//Square structure
+type Square struct {
+	start Point
+	a     uint
+}
+
+//End method
+func (p *Square) End() (int, int) {
+	var xend = p.start.x + int(p.a)
+	var yend = p.start.y - int(p.a)
+	return xend, yend
+}
+
+//Perimeter method
+func (p *Square) Perimeter() uint { //
+	return p.a * uint(4) //
+}
+
+//Area method
+func (p *Square) Area() uint { /**/
+	return (p.a * p.a)
+}
+
+func main() {
+	s := Square{Point{3, -3}, 5}
+	fmt.Println(s.End())
+	fmt.Println(s.Perimeter())
+	fmt.Println(s.Area())
 }
