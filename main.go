0e880fbbeb1c0b5b65b565e372ffbdbef0f7424d
stevenstr <stephenzhavrid@gmail.com>
2019-11-16 17:21:38 +0300

    Update main.go
    Just a program with a factorial search function

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 /*
  *Author: Stefan
- *Date: 11/13/2019
- *Task
+ *Date: 11/16/2019
+ *Task: Home work 2.1 Implement - func factorial(i uint) uint - function without using recursion
+ *Examples: Factorial(0) = 1; For enother: Factorial(3) = 1 * 2 * 3 = 6; Factorial(5) = 1 * 2 * 3 * 4 * 5 = 120;
 **/
 
 package main
 
 import "fmt"
 
+func factorial(i uint) uint {
+
+	if i == 0 {
+		return 1
+	}
+
+	var result uint = 1
+	var a uint = 2
+
+	for a <= i {
+		result *= a
+		a++
+	}
+
+	return result
+}
+
 func main() {
-
-	fmt.Println("Hello Golang Worldff!")
-
+	fmt.Println(factorial(5))
 }
