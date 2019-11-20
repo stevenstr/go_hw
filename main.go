d3faad231e052ecf682d4d68c92e1cc1f7aba5ab
stevenstr <stephenzhavrid@gmail.com>
2019-11-20 15:31:40 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  *Author: Stefan
  *Date: 11/20/2019
- *Last changes: 11/20/2019 15.10
+ *Last changes: 11/20/2019 15.33
  *Task: Write max([]string) string function that returns the longest word from the slice of strings (the first if there are more than one).
  *			Input -> ("one", "two", "three")
  *			Output -> ("three")
@@ -12,9 +12,9 @@ package main
 import "fmt"
 
 func max(sl []string) string {
-	var length int = 0
+	var length int //var length int = 0
 	var N = len(sl)
-	var result string = ""
+	var result string //var result string = ""
 
 	// if there are several elements of the same length, then it will return the first
 	for i := 0; i < N; i++ {
