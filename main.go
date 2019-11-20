d1b30d3bb5fa7001c4a48c80f0686e98bda8afda
stevenstr <stephenzhavrid@gmail.com>
2019-11-20 15:33:07 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  *Author: Stefan
  *Date: 11/20/2019
- *Last changes: 11/20/2019 13.45
+ *Last changes: 11/20/2019 15.33
  *Task: Write reverse([]int64) []int64 function that returns the copy of the original slice in reverse order. The type of elements is int64.
  *						Input -> (1, 2, 5, 15)
  *						Output -> (15, 5, 2, 1)
@@ -18,7 +18,7 @@ func reverse(sl []int64) []int64 {
 	//Just make copy with original len and cap
 	var cpSl = make([]int64, n, c)
 
-	var start = 0
+	var start int //var start = 0
 	var end = n - 1
 	//Just  do it
 	for i := n - 1; i >= 0; i-- {
