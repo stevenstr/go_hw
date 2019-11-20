6751c24ffc20a4986def492696125149babbbef4
stevenstr <stephenzhavrid@gmail.com>
2019-11-20 15:03:09 +0300

    Update main.go
    Implement printSorted(map[int]string) function that prints map values sorted in order of increasing keys.

--- main.go
+++ main.go
@@ -1,38 +1,39 @@
 /*
  *Author: Stefan
  *Date: 11/20/2019
- *Last changes: 11/20/2019 13.45
- *Task: Write reverse([]int64) []int64 function that returns the copy of the original slice in reverse order. The type of elements is int64.
- *						Input -> (1, 2, 5, 15)
- *						Output -> (15, 5, 2, 1)
+ *Last changes: 11/20/2019 15.05
+ *Task: Implement printSorted(map[int]string) function that prints map values sorted in order of increasing keys.
+ *					Input -> {2: "a", 0: "b", 1: "c"}
+ *					Output -> ["b", "c", "a"]
 **/
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-//reverse function
-func reverse(sl []int64) []int64 {
-	var n = len(sl)
-	var c = cap(sl)
-	//Just make copy with original len and cap
-	var cpSl = make([]int64, n, c)
+//printSorted function
+func printSorted(m map[int]string) {
 
-	var start = 0
-	var end = n - 1
-	//Just  do it
-	for i := n - 1; i >= 0; i-- {
-		cpSl[start] = sl[end]
-		start++
-		end--
+	var sl []int
+
+	for i := range m {
+		sl = append(sl, i)
 	}
-	return cpSl
+	//Is the easiest way to do it
+	sort.Ints(sl)
+
+	for _, i := range sl {
+		fmt.Print(m[i], " ")
+	}
+	fmt.Println()
 }
 
 func main() {
-	var sl = []int64{1, 2, 5, 15, 20, 33, 1}
-
-	fmt.Println("original: ", sl)
-	fmt.Println()
-	fmt.Println("reversed: ", reverse(sl))
+	m := map[int]string{2: "a", 0: "b", 1: "c", 5: "g"}
+	printSorted(m)
+	m = map[int]string{10: "aa", 0: "bb", 500: "cc"}
+	printSorted(m)
 }
