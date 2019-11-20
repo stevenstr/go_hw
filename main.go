8700fbce25a447119954c7477905f9929d58b549
stevenstr <stephenzhavrid@gmail.com>
2019-11-20 12:49:16 +0300

    Update main.go
    Implement average([]int) float64 function that returns an average value of slice (sum / N)

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
+/*
+ *Author: Stefan
+ *Date: 11/20/2019
+ *Last changes: 11/20/2019 12.50
+ *Task: Implement average([]int) float64 function that returns an average value of slice (sum / N)
+**/
+
 package main
 
-import (
-"fmt"
-)
+import "fmt"
+
+func average(sl []int) float64 {
+	var N int = len(sl)
+	var sum int = 0
+	for i := 0; i < N; i++ {
+		sum += sl[i]
+	}
+	return float64(sum) / float64(N)
+}
 
 func main() {
-  fmt.Println("Hello world!")
+	var sl = []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(average(sl))
 }
