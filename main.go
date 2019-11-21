aaf5f43f5401b11e1e7b6880110fd844050e8a65
stevenstr <stephenzhavrid@gmail.com>
2019-11-21 13:21:20 +0300

    Update main.go
    Using if utf8.RuneCountInString(sl[i]) > utf8.RuneCountInString(result) {

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  *Author: Stefan
  *Date: 11/20/2019
- *Last changes: 11/20/2019 15.33
+ *Last changes: 11/21/2019 13.21
  *Task: Write max([]string) string function that returns the longest word from the slice of strings (the first if there are more than one).
  *			Input -> ("one", "two", "three")
  *			Output -> ("three")
@@ -9,17 +9,20 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func max(sl []string) string {
-	var length int //var length int = 0
+	//var length int = 0 //var length int = 0
 	var N = len(sl)
 	var result string //var result string = ""
 
 	// if there are several elements of the same length, then it will return the first
 	for i := 0; i < N; i++ {
-		if len(sl[i]) > length {
-			length = len(sl[i])
+		if utf8.RuneCountInString(sl[i]) > utf8.RuneCountInString(result) {
+			//length = len(sl[i])
 			result = sl[i]
 		}
 	}
