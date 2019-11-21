741c78f0b840172e1b2a85bb79a6898b78b5a265
stevenstr <stephenzhavrid@gmail.com>
2019-11-21 13:12:12 +0300

    Update main.go
    Added verification that the cut length is greater than 0

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  *Author: Stefan
- *Date: 11/20/2019
- *Last changes: 11/20/2019 12.50
+ *Date: 11/21/2019
+ *Last changes: 11/20/2019 13.12
  *Task: Implement average([]int) float64 function that returns an average value of slice (sum / N)
 **/
 
@@ -10,6 +10,10 @@ package main
 import "fmt"
 
 func average(sl []int) float64 {
+	if len(sl) == 0 {
+		fmt.Println("Empty slice")
+		return 0
+	}
 	var N int = len(sl)
 	var sum int //var sum int = 0
 	for i := 0; i < N; i++ {
@@ -20,5 +24,7 @@ func average(sl []int) float64 {
 
 func main() {
 	var sl = []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(average(sl))
+	sl = []int{}
 	fmt.Println(average(sl))
 }
