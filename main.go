6e466580791db130dfcac53414d47b067215aa9a
stevenstr <stephenzhavrid@gmail.com>
2019-11-27 16:08:25 +0300

    Update

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+ *Author: Stefan
+ *Date: 11/24/2019
+ *Last changes: 11/26/2019 15.10
+ *Task: Implement a HelloWorld program that will print a smiley us
+party dependency (https://github.com/kyokomi/emoji).
+**/
+
+package main
+
+import "github.com/kyokomi/emoji"
+
+func main() {
+	emoji.Println("Hello world :smile:!")
+}
