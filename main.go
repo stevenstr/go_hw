5433fcdca9af16f21422e44946015cc2fd592a65
Ste Zha <stephenzhavrid@gmail.com>
2019-12-11 01:07:44 +0300

    Update
    second method with good  readability

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /**
  *Author: Stefan
  *Date: 12/10/2019
- *Last changes: 12/10/2019 17.00
+ *Last changes: 12/11/2019 01.05
  *Task:
  */
 
@@ -16,38 +16,39 @@ import (
 
 //Response struct
 type Response struct {
-	Host       string      `json:"host"`
-	UserAgent  string      `json:"user_agent"`
-	RequestURI string      `json:"request_uri"`
-	Header     http.Header `json:"headers"`
+	Host       string `json:"host"`
+	UserAgent  string `json:"user_agent"`
+	RequestURI string `json:"request_uri"`
+	HeaderU    string `json:"headers"`
+	HeaderA    string `json:"Accept"`
 }
 
-//Our handler
-func handler(w http.ResponseWriter, r *http.Request) {
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
-	//1 method
-	//json.NewEncoder(w).Encode(Response{r.Host, r.UserAgent(), r.RequestURI, r.Header})
+	p := &Response{
+		Host:       r.Host,
+		UserAgent:  r.UserAgent(),
+		RequestURI: r.URL.String(),
+		HeaderU:    r.Header.Get("User-Agent"),
+		HeaderA:    r.Header.Get("Accept"),
+	}
 
-	//2 method
-	p := &Response{r.Host, r.UserAgent(), r.RequestURI, r.Header}
-	json.Marshal(p)
-	fmt.Fprintln(w, p)
-	/* 3 method
-	fmt.Fprintln(w, "URL:", r.URL.String())
-	fmt.Fprintln(w, "host:", r.Host)
-	fmt.Fprintln(w, "user_agent:", r.UserAgent())
-	fmt.Fprintln(w, "Accept:", r.Header.Get("Accept"))
-	fmt.Fprintln(w, "User-Agent:", r.Header.Get("User-Agent"))
-	*/
+	json.NewEncoder(w).Encode(p.Host)
+	fmt.Fprintln(w)
+	json.NewEncoder(w).Encode(p.UserAgent)
+	fmt.Fprintln(w)
+	json.NewEncoder(w).Encode(p.RequestURI)
+	fmt.Fprintln(w)
+	json.NewEncoder(w).Encode(p.HeaderU)
+	fmt.Fprintln(w)
+	json.NewEncoder(w).Encode(p.HeaderA)
+	fmt.Fprintln(w)
 }
 
 func main() {
-	//обработчики
-	//http.HandleFunc("/", handler)
-	//напишет в консоли где запущен сервак
-
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
+	mux.HandleFunc("/", jsonHandler)
 
 	server := http.Server{
 		Addr:         ":8080",
@@ -57,6 +58,5 @@ func main() {
 	}
 
 	fmt.Println("starting server at: 8080")
-	//запускает сервер на порту 8080
 	server.ListenAndServe()
 }
