248562d16f43e9ead81c8d03834691afd8c6dbb4
Ste Zha <stephenzhavrid@gmail.com>
2019-12-18 15:09:35 +0300

    Correction megration
    Kolya thanks!

--- megration/0001.init.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.contsct (
-    id integer NOT NULL,
-    first_name character varying(255) NOT NULL,
-    last_name character varying(255) NOT NULL,
-    phone character varying(255) NOT NULL,
-    email character varying(255) NOT NULL
-);
