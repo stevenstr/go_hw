48d4c5e31ecaee5634f6040a405d05f467535f07
Ste Zha <stephenzhavrid@gmail.com>
2020-01-08 16:29:22 +0300

    Just lose one file
    Fix

---
+++ app/auth.go
@@ -0,0 +1,89 @@
+/*
+ *Author: Stefan
+ *Date: 12/25/2019
+ *Last changes: 01/08/2019
+ *Task: JWT
+ */
+
+package app
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/stevenstr/models"
+	u "github.com/stevenstr/utils"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+//JwtAuthentication var
+var JwtAuthentication = func(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		//                           прописать свои пути
+
+		notAuth := []string{"/api/user/register", "/api/user/login"}
+		requestPath := r.URL.Path
+
+		for _, value := range notAuth {
+
+			if value == requestPath {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		response := make(map[string]interface{})
+		tokenHeader := r.Header.Get("Authorization")
+
+		if tokenHeader == "" {
+			response = u.Message(false, "Missing auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		splitted := strings.Split(tokenHeader, " ")
+		if len(splitted) != 2 {
+			response = u.Message(false, "Invalid/Malformed auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		tokenPart := splitted[1]
+		tk := &models.Token{}
+
+		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+
+		if err != nil {
+			response = u.Message(false, "Malformed authentication token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		if !token.Valid {
+			response = u.Message(false, "Token is not valid.")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		fmt.Sprintf("User %", tk.UserID)
+		ctx := context.WithValue(r.Context(), "user", tk.UserID)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
